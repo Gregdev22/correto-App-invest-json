357a6aeb11721d07a56f40e046985e01955dd7bb
Gregdev22 <103840468+Gregdev22@users.noreply.github.com>
2022-07-04 19:10:13 -0300

    Verificar e inserir novo usuario ok
    Verificação de usuario pelo id e adicionar novo usuario tambem. Iniciar tarefa 06 (logar o usuario com sua senha e id ou cadastrar um usuario)

--- basededados.py
+++ basededados.py
@@ -6,35 +6,26 @@ class BaseDeDados():
         with open("dados.json", "r") as file:
             dados_dict_json = file.read()  # --> Aqui é um arquivo json
             self.dados = json.loads(dados_dict_json)  # --> Aqui é um dicionario python
+            print(self.dados.keys()) # --> Mostra as chaves do dicionario
 
-    # def __init__(self):
-    #     self.dados = {}
+    def verificar_usuario(self):
+        self.id = input("Insira o id do usuario")
+        if self.id in self.dados.keys():
+            print(f'Exibindo dados do usuario de id {self.id}.')
+            # Chamar uma função de carregar dados do usuario e/ou de alterar dados do usuario
+        else:
+            # Rodar ALGUMAS funções da classe BaseDeDados
+            self.inserir_dados_usuario()
+            self.adicionar_inv()
 
     def inserir_dados_usuario(self):
 
-        #print(self.dados)
-        self.carteira = {}
         self.nome = input("Insira seu nome")
         self.sobrenome = input("Insira seu sobrenome")
         self.email = input("Insira seu email")
         self.senha = input("Insira seu senha")
-        self.id = input("Insira seu id")
-        self.dict_dados_usuario = {
-            "Nome":self.nome,
-            "Sobrenome":self.sobrenome,
-            "Email":self.email,
-            "id":self.id
-        }
-        #self.dados.update(self.dict_dados_usuario)
-
-    def verificar_usuario(self):
-        id = input("Insira o id do usuario")
-        if id in self.dados['id']:
-            print(f'usuario {self.dados["Nome"]} já está cadastrado com o id: {self.dados["id"]}')
-        else:
-            print('usuario nao encontrado')
-
-
+        # ---> Como eu chamo o id passado na função verificar usuario ?
+        # R= Tive que trocar 'id' por 'self.id'. O self.id fica disponivel para uso de todas as funções da classe.
 
     def adicionar_inv(self):
         tipo_inv = input("insira RF ou RV")
@@ -44,33 +35,43 @@ class BaseDeDados():
             renda_fixa_indexador = input("Insira o indexador: IPCA, CDI, PRE")
             renda_fixa_valor = input("Insira a valor investido")
             self.dict_renda_fixa = {"tipo":tipo_inv, "nome":renda_fixa_nome,"indexador":renda_fixa_indexador,"valor":renda_fixa_valor}
-            #self.dict_renda_fixa.update({"tipo":tipo_inv, "nome":renda_fixa_nome,"indexador":renda_fixa_indexador,"valor":renda_fixa_valor})
-            # Como adiciono um dicionarios ao dicionario carteira da classe usuario? R= abaixo.
+            # Como adiciono um dicionario a UM dicionario? R= abaixo.
             self.carteira.update(self.dict_renda_fixa)
-            self.dict_carteira = {"Carteira": self.carteira}
-            self.dados.update(self.dict_carteira)  # esse dicionario deve ficar separado das demais info do usuario
-            # print(self.carteira)
-            #print(self.dados)
+            self.dict_dados_usuario = {
+                "Nome": self.nome,
+                "Sobrenome": self.sobrenome,
+                "Email": self.email,
+                "id": self.id,
+                "CARTEIRA": self.carteira
+            }
+            print(self.dict_dados_usuario)
+            self.dados[self.id] = (self.dict_dados_usuario)
+            print(self.dados)
+
             dados_json = json.dumps(self.dados, indent=True)
             with open("dados.json", "w+") as file:
                 file.write(dados_json)
-            #print(dados_json)
         else:
             self.carteira = {}
             renda_variavel_tipo = input("Insira o tipo da renda variavel: Acao, FII ou ETF")
             renda_variavel_papel = input("Insira o nome do papel")
             renda_variavel_valor = input("Insira a valor investido")
             self.dict_renda_variavel = {"Inv":tipo_inv, "tipo":renda_variavel_tipo,"papel":renda_variavel_papel, "valor":renda_variavel_valor}
-            # Como adiciono um dicionarios ao dicionario carteira da classe usuario? R= abaixo.
             self.carteira.update(self.dict_renda_variavel)
-            self.dict_carteira = {"Carteira": self.carteira}
-            self.dados.update(self.dict_carteira) # esse dicionario deve ficar separado das demais info do usuario
-            #print(self.carteira)
-            #print(self.dados)
+            self.dict_dados_usuario = {
+                "Nome": self.nome,
+                "Sobrenome": self.sobrenome,
+                "Email": self.email,
+                "id": self.id,
+                "CARTEIRA": self.carteira
+            }
+            print(self.dict_dados_usuario)
+            self.dados[self.id] = (self.dict_dados_usuario)
+            print(self.dados)
+
             dados_json = json.dumps(self.dados, indent=True)
             with open("dados.json", "w+") as file:
                 file.write(dados_json)
-            #print(dados_json)
 
 
 
